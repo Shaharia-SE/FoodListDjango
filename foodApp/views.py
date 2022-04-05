6c2ef69a1bd8c7c754986f9a57eae3a76ff9d073
Shaharia-SE <shahariarony38@gmail.com>
2022-04-05 22:24:02 +0600

    admin

--- foodApp/views.py
+++ foodApp/views.py
@@ -7,7 +7,7 @@ from django.template import loader
 # create view
 def index(request):
     item_list = Item.objects.all()
-    #template = loader.get_template('food/index.html')
+    # template = loader.get_template('food/index.html')
     contex = {
         'item_list': item_list,
 
@@ -17,3 +17,13 @@ def index(request):
 
 def item(request):
     return HttpResponse('<h1>This is an item view<h1>')
+
+
+def detail(request, item_id):
+    item = Item.objects.get(pk=item_id)
+    context = {
+        'item': item,
+
+    }
+
+    return render(request, 'food/detail.html', context)
