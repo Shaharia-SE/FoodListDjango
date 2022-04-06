57e241614c430220a2916a36ddedde3fd73ad263
Shaharia-SE <shahariarony38@gmail.com>
2022-04-07 00:40:23 +0600

    admin

--- foodApp/urls.py
+++ foodApp/urls.py
@@ -5,7 +5,15 @@ app_name = 'food'
 urlpatterns = [
     # /food/
     path('', views.index, name='index'),
-    # /food/
+    # /food/1
     path('<int:item_id>/', views.detail, name='detail'),
     path('item/', views.item, name='item'),
+    # Add item
+    path('add', views.create_item, name='create_item'),
+
+    # edit
+    path('update/<int:id>/', views.update_item, name='update_item'),
+    # delete
+    path('delete/<int:id>/', views.delete_item, name='delete_item'),
+
 ]
--- foodApp/views.py
+++ foodApp/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Item
 from django.template import loader
+from .forms import ItemForm
 
 
 # create view
@@ -13,6 +14,12 @@ def index(request):
 
     }
     return render(request, 'food/index.html', contex)
+
+
+class IndexClassView(ListView):
+    model = Item;
+    template_name = 'food/index.html'
+    context_object_name = 'item_list'
 
 
 def item(request):
@@ -27,3 +34,28 @@ def detail(request, item_id):
     }
 
     return render(request, 'food/detail.html', context)
+
+
+def create_item(request):
+    form = ItemForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('food:index')
+    return render(request, 'food/item-form', {'form': form})
+
+
+def update_item(request, id):
+    item = Item.objects.get(id=id)
+    form = ItemForm(redirect.POST or None, instance=item)
+    if form.is_valid():
+        form.save()
+        return redirect('food:index')
+    return render(request, 'food/item-form.html', {'form': form, 'item': item})
+
+
+def delete_item(request, id):
+    item = Item.objects.get(id=id)
+    if request.method == "POST":
+        item.delete()
+        return redirect('food:index')
+    return render(request, 'food/item-delete.html', {'item': item})
