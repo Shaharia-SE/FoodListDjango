3fff77c0b63c60cc0bf1b16e7f9d31ca210e9f63
Shaharia-SE <shahariarony38@gmail.com>
2022-04-10 11:39:14 +0600

    admin

--- FoodListDjango/urls.py
+++ FoodListDjango/urls.py
@@ -23,7 +23,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('foodApp.urls')),
+    path('food/', include('foodApp.urls')),
     path('register/', user_views.register, name='register'),
     path('login/', authentication_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', authentication_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
